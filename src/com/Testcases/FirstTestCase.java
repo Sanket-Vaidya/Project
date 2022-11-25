c158b59912d3fbe18462f7cc4f83b517b7e05197
Sanket.Vaidya <sanket2733@gmail.com>
2022-11-25 11:44:11 +0530

    COmmiting

---
+++ src/com/Testcases/FirstTestCase.java
@@ -0,0 +1,12 @@
+package com.Testcases;
+
+import org.testng.annotations.Test;
+
+public class FirstTestCase {
+	
+	@Test
+	public void TestCase() {
+		System.out.println("Executing sample test");
+	}
+
+}
---
+++ src/com/Testcases/SecondTestcase.java
@@ -0,0 +1,12 @@
+package com.Testcases;
+
+import org.testng.annotations.Test;
+
+public class SecondTestcase {
+	
+	@Test
+	public void testcasesecond() {
+		System.out.println("Executing test");
+	}
+
+}
